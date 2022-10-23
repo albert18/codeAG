9964111f7d1db979547aa863faeb982da414ff12
albert18 <erertech@gmail.com>
2022-10-23 11:53:11 +0300

    update

--- clips/src/app/nav/nav.component.ts
+++ clips/src/app/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ModalService } from '../services/modal.service';
 import { AuthService } from '../services/auth.service';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -12,8 +13,9 @@ export class NavComponent implements OnInit {
   constructor(
     public modal: ModalService,
     public auth: AuthService,
-    private afAuth: AngularFireAuth
-  ) { 
+    private afAuth: AngularFireAuth,
+    private router: Router
+  ) {
   }
 
   ngOnInit(): void {
@@ -29,6 +31,8 @@ export class NavComponent implements OnInit {
     $event.preventDefault()
 
     await this.afAuth.signOut()
-  }
 
+    await this.router.navigateByUrl('/')
+
+  }
 }
