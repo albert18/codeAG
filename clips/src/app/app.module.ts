40de0e33e11e741360cc48455d46859198e93d2c
albert18 <erertech@gmail.com>
2022-10-26 21:52:17 +0300

    update

--- clips/src/app/app.module.ts
+++ clips/src/app/app.module.ts
@@ -12,13 +12,15 @@ import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component'
 import { VideoModule } from './video/video.module';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
