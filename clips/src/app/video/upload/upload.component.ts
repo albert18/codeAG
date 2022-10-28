3ba388cc1078d90e1d2432851a3f1b6af7986d7b
albert18 <erertech@gmail.com>
2022-10-28 20:56:51 +0300

    update

--- clips/src/app/video/upload/upload.component.ts
+++ clips/src/app/video/upload/upload.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class UploadComponent implements OnInit {
   isDragover = false
   file: File | null = null
+  nextStep = false
 
   constructor() { }
 
@@ -23,7 +24,7 @@ export class UploadComponent implements OnInit {
       return
     }
 
-    console.log(this.file)
+    this.nextStep = true
   }
 
 }
