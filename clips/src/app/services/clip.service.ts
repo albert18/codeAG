646d69869b58ebbfba6bb1f19dea855a7e63baca
albert18 <erertech@gmail.com>
2022-11-04 22:57:28 +0300

    update

--- clips/src/app/services/clip.service.ts
+++ clips/src/app/services/clip.service.ts
@@ -5,12 +5,15 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { switchMap, map } from 'rxjs/operators';
 import { of, BehaviorSubject, combineLatest } from 'rxjs';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { getLocaleDayPeriods } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClipService {
   public clipsCollection: AngularFirestoreCollection<IClip>
+  pageClips: IClip[] = []
+  pendingReq = false
 
   constructor(
     private db: AngularFirestore,
@@ -65,6 +68,36 @@ export class ClipService {
     await screenshotRef.delete()
 
     await this.clipsCollection.doc(clip.docID).delete()
+  }
 
+  async getClips() {
+    if (this.pendingReq) {
+      return
+    }
+
+    this.pendingReq = false
+    let query = this.clipsCollection.ref.orderBy(
+      'timestamp', 'desc'
+    ).limit(6)
+
+    const { length } = this.pageClips
+
+    if (length) {
+      const lastDocID = this.pageClips[length - 1].docID
+      const lastDoc = await this.clipsCollection.doc(lastDocID).get().toPromise()
+
+      query = query.startAfter(lastDoc)
+    }
+
+    const snapshot = await query.get()
+
+    snapshot.forEach(doc => {
+      this.pageClips.push({
+        docID: doc.id,
+        ...doc.data()
+      })
+    })
+
+    this.pendingReq = false
   }
 }
