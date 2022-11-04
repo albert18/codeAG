e3d3defaf32a1afe65e6cff3ed3eae34f960b367
albert18 <erertech@gmail.com>
2022-11-04 22:42:12 +0300

    update

--- clips/src/app/clip-list/clip-list.component.ts
+++ clips/src/app/clip-list/clip-list.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-clip-list',
   templateUrl: './clip-list.component.html',
   styleUrls: ['./clip-list.component.css']
 })
-export class ClipListComponent implements OnInit {
+export class ClipListComponent implements OnInit, OnDestroy {
 
   constructor() { }
 
   ngOnInit(): void {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = () => {
+    const { scrollTop, offsetHeight} = document.documentElement
+    const { innerHeight } = window
+    const bottomOfWindow = Math.round(scrollTop) + innerHeight === offsetHeight
+
+    if (bottomOfWindow) {
+
+    }
   }
 
 }
