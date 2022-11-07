648abb6602a83790c9a236325df5c71df483eb23
albert18 <erertech@gmail.com>
2022-11-07 22:15:14 +0300

    update

--- clips/src/app/pipes/fb-timestamp.pipe.ts
+++ clips/src/app/pipes/fb-timestamp.pipe.ts
@@ -2,7 +2,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 import firebase from 'firebase/compat';
 import { DatePipe } from '@angular/common';
 
-
 @Pipe({
   name: 'fbTimestamp'
 })
@@ -10,7 +9,11 @@ export class FbTimestampPipe implements PipeTransform {
 
   constructor(private datePipe: DatePipe) {}
 
-  transform(value: firebase.firestore.FieldValue) {
+  transform(value: firebase.firestore.FieldValue | undefined) {
+    if (!value) {
+      return ''
+    }
+
     const date = (value as firebase.firestore.Timestamp).toDate()
 
     return this.datePipe.transform(date, 'mediumDate');
