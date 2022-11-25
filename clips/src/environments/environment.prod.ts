3727c745784c5a72cf0ce246a80f01fadaac0dab
albert18 <erertech@gmail.com>
2022-11-25 20:55:11 +0300

    remove key

--- clips/src/environments/environment.prod.ts
+++ clips/src/environments/environment.prod.ts
@@ -1,12 +1,7 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDTktNXDweuuAZ3RdeMbsbtyFwnh65wk1I",
-    authDomain: "test-clips.firebaseapp.com",
-    projectId: "test-clips",
-    storageBucket: "test-clips.appspot.com",
-    messagingSenderId: "1043572567074",
-    appId: "1:1043572567074:web:f5355a6677d1e02a7eae7b"
+
 
   }
 };
